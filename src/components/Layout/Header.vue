e8ebed78edd736dfbd3c157b4b05840e268ee0d0
Sergio González <sglinares@minsait.es>
2020-11-07 12:03:21 +0100

    Initial layout

---
+++ src/components/Layout/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-app-bar
+    app
+    color="white"
+    flat
+  >
+    <v-toolbar-side-icon>
+      <v-img
+        :src="require('@/assets/beer-logo.svg')"
+        contain
+        height="40"
+      />
+    </v-toolbar-side-icon>
+    <v-toolbar-title>
+      Henequet Brewery
+    </v-toolbar-title>
+    <!-- <v-container class="py-0 fill-height">
+      <v-btn
+        v-for="link in links"
+        :key="link"
+        text
+      >
+        {{ link }}
+      </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-responsive max-width="260">
+        <v-text-field
+          dense
+          flat
+          hide-details
+          rounded
+          solo-inverted
+        ></v-text-field>
+      </v-responsive>
+    </v-container> -->
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data: () => ({
+    links: []
+  })
+}
+</script>
---
+++ src/components/Layout/SideMenu.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-sheet rounded="lg">
+    <v-list color="transparent">
+      <v-list-item
+        v-for="link in links"
+        :key="link.text"
+        link
+        :disabled="link.disabled"
+        :to="link.href"
+      >
+        <v-list-item-content>
+          <v-list-item-title>
+            {{ link.text }}
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+  name: 'SideMenu',
+  data: () => ({
+    links: [
+      {
+        text: 'Cervezas',
+        href: 'beers',
+        disabled: false
+      },
+      {
+        text: 'Vinos',
+        href: 'wines',
+        disabled: true
+      },
+      {
+        text: 'Licores',
+        href: 'spirits',
+        disabled: true
+      },
+      {
+        text: 'Combinados',
+        href: 'cocktails',
+        disabled: true
+      },
+      {
+        text: 'Zumos',
+        href: 'juices',
+        disabled: true
+      }
+    ]
+  })
+}
+</script>
